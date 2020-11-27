7f9611ecf7b2c43b5e36175c28062afe8d1fd9c6
Gareth Tribello <gareth.tribello@gmail.com>
2020-11-27 21:33:19 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+def isInNaturals( a, b ) :
+  # Your code goes here
+  
+
+print("The statement 3-2 is in the set of natural numbers is", isInNaturals(3,2) )
+print("The statement 3-3 is in the set of natural numbers is", isInNaturals(3,3) )
+print("The statement 3-5 is in the set of natural numbers is", isInNaturals(3,5) )
+  
+  
