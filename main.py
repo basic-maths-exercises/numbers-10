ad1a721baa3545ac93c61afc589e2d926d09b296
Gareth Tribello <gareth.tribello@gmail.com>
2021-02-03 18:29:40 +0000

    Setup answers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def isInNaturals( a, b ) :
   # Your code goes here
-  
+  if a<b : return 0
+  return 1 
 
 print("The statement 3-2 is in the set of natural numbers is", isInNaturals(3,2) )
 print("The statement 3-3 is in the set of natural numbers is", isInNaturals(3,3) )
